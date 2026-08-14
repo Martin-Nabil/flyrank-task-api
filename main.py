fc0d694d1c3f043ee606cbd836c75b24602a6a7c
Martin <mk202301498@gaf.ac>
2026-08-14 07:13:10 +0300

    Stage 0: setup server and supabase client

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 load_dotenv()
+from supabase import create_client, Client
+
+SUPABASE_URL = os.environ["SUPABASE_URL"]
+SUPABASE_KEY = os.environ["SUPABASE_KEY"]
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI(
     title="Task API",
